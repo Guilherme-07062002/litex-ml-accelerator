4711861ec4ec9c5f5bb1c1a030181a558afcad4f
Guilherme-07062002 <guilherme762002@gmail.com>
2025-11-25 21:46:09 -0300

    refactor: remover arquivos e funções não utilizados, simplificar Makefile e adicionar firmware principal

---
+++ hardware/ip/firmware.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <console.h>
+#include <system.h>
+
+
+// -------------------------------------------------------------
+// main: inicialização e loop principal
+// -------------------------------------------------------------
+int main(void) {
+    printf("\n---------------- LiteX BIOS --------------\n");
+    printf(  "Tarefa 06" );
+    
+
+    return 0;
+}
