3a3fc982152078ba3dd60158cfa4b018104c2d27
Guilherme-07062002 <guilherme762002@gmail.com>
2025-11-27 20:26:48 -0300

    chore: remove arquivos desnecessários

--- hardware/ip/inference.h
+++ hardware/ip/inference.h
@@ -1,26 +1,11 @@
-/* Copyright 2023 LiteX ML Accelerator Project
- * Implementação da inferência TensorFlow Lite Micro para hello_world
- */
+// Declarações da função de inferência
+#ifndef INFERENCE_H
+#define INFERENCE_H
 
-#ifndef INFERENCE_H_
-#define INFERENCE_H_
+#include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+// Função principal de inferência
+// Retorna o valor predito pela rede neural para a entrada x
+float predict(float x);
 
-// Inicializa o modelo TFLite Micro
-void inference_init(void);
-
-// Executa uma inferência com o valor de entrada x (radianos)
-// Retorna o valor de saída (seno de x), no intervalo [-1, 1]
-float inference_run(float x_value);
-
-// Converte valor de saída do modelo (0.0 a 1.0) para padrão de LEDs (0-255)
-unsigned char inference_output_to_led_pattern(float output_value);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif  // INFERENCE_H_
+#endif // INFERENCE_H
